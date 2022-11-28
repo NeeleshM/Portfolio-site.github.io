d4ed80045f2032d8f98091396faae29691fada50
Neelesh <cse21018@glbitm.ac.in>
2022-11-28 21:34:34 +0530

    first commit

---
+++ frontend/src/components/SocialMedia.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BsGithub, BsInstagram, BsLinkedin } from 'react-icons/bs';
+
+
+const SocialMedia = () => (
+  <div className="app__social">
+    <div>
+    <a href = "https://github.com/NeeleshM" target="blank" className="BsGithub">
+      <BsGithub />
+      </a>
+    </div>
+    <div>
+      
+      <a href = "https://www.linkedin.com/in/neelesh-maurya-32537b204" target="blank" className="BsLinkedin">
+      <BsLinkedin />
+      </a>
+    </div>
+   
+    <div>
+    <a href = "https://www.instagram.com/neeleshm_" target="blank" className="BsInstagram">
+      <BsInstagram />
+      </a>
+    </div>
+  </div>
+);
+
+export default SocialMedia;
